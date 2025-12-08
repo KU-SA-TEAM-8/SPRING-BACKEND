f104f9850943616f9c2fa92eadf51bc4d523f76f
chanwookK <kan0202@naver.com>
2025-12-09 01:14:27 +0900

    [Feat] RabbitMQ Message Converter 추가

--- src/main/java/sa_team8/scoreboard/Infrastructure/config/RabbitConfig.java
+++ src/main/java/sa_team8/scoreboard/Infrastructure/config/RabbitConfig.java
@@ -1,6 +1,10 @@
 package sa_team8.scoreboard.Infrastructure.config;
 
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +19,17 @@ public class RabbitConfig {
   @Bean
   public TopicExchange scoreboardExchange() {
     return new TopicExchange(EXCHANGE);
+  }
+
+  @Bean
+  public MessageConverter jsonMessageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+  @Bean
+  public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    RabbitTemplate template = new RabbitTemplate(connectionFactory);
+    template.setMessageConverter(jsonMessageConverter());
+    return template;
   }
 }
