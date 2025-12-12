330fcc426f467efd9dafd1bd082ca40326c36284
kangchanwook <kan0202@naver.com>
2025-12-12 15:27:18 +0900

    [Fix] Merge 버그 수정

--- src/main/java/sa_team8/scoreboard/global/config/CorsConfig.java
+++ src/main/java/sa_team8/scoreboard/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package sa_team8.scoreboard.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
