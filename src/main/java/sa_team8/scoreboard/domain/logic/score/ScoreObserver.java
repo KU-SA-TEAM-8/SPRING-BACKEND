999a7bd93f889c2201f297550a6b3532fb9a4ee9
chanwookK <kan0202@naver.com>
2025-12-21 18:29:27 +0900

    [Remove] 사용하지 않는 클래스 제거

--- src/main/java/sa_team8/scoreboard/domain/logic/score/ScoreObserver.java
+++
@@ -1,9 +0,0 @@
-package sa_team8.scoreboard.domain.logic.score;
-
-import java.util.UUID;
-import sa_team8.scoreboard.domain.entity.Score;
-import sa_team8.scoreboard.domain.logic.history.ScoreEvent;
-
-public interface ScoreObserver {
-  void onScoreUpdated(ScoreEvent event);
-}
